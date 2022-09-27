2f60f0fdd4d895bc227570b69274cecbb4991dcb
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-09-27 18:05:00 +0200

    first commit

---
+++ src/test/java/fr/hndgy/videostream/VideoStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hndgy.videostream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
